2437f46843937256fde509cfcdedf6c470e0b77a
Roy Matthew Ramos <roymr@uci.edu>
2020-06-29 15:04:03 -0700

    Sketching functionality works

---
+++ etch.js
@@ -0,0 +1,32 @@
+const container = document.querySelector("#container");
+
+// let x = document.createElement('p');
+// x.textContent = "YUH";
+
+// container.appendChild(x);
+
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            let box = document.createElement('div');
+            box.setAttribute('class', "grid");
+            box.setAttribute("style", `width: ${600/x}px; height:${600/x}px`);
+            container.append(box);
+        };
+    };
+
+
+    // box.style.height = 600/x + "px";
+    // box.style.width = 600/x + "px";
+};
+
+createGrid(16);
+
+
+document.querySelectorAll('.grid').forEach(i => {
+    i.addEventListener("mouseover", mouseOver)
+});
+
+function mouseOver(){
+    this.classList.add("hovered");
+}
